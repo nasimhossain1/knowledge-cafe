6cbdefd88882f0a46cc1d6e9451618176a332658
nasimhossain1 <nasimhossain1@gmail.com>
2025-09-30 23:39:33 +0600

    cafe data

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+import './App.css'
+import Header from './components/Headers/Header'
+import Blogs from './components/Blogs/Blogs'
+
+function App() {
+  
+
+  return (
+    <>  
+     <Header></Header>
+     <Blogs></Blogs>
+    </>
+  )
+}
+
+export default App
